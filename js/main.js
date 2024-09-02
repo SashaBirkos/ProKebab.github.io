e21663b7eacb6d3bfdea5a09b6882e2d5a111d1e
Sasha Birkos <sasha.birkos@mail.ru>
2024-09-02 09:48:47 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let bg = document.querySelector('.banner__shawarma');
+window.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    bg.style.transform = 'translate(-' + x * 60 + 'px, -' + y * 50 + 'px)';
+});
+
+
+const menuBtn = document.querySelector('.menu__btn-phone');
+const menuMobile = document.querySelector('.menu__items');
+
+menuBtn.addEventListener('click', () => {
+    menuMobile.classList.toggle('menu--open');
+});
+
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
